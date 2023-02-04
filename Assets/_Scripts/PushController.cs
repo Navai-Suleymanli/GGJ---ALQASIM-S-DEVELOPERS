4b41f899c1ca72b70cecdd055c33d629b88b24cd
Murad <72296224+Musayev442@users.noreply.github.com>
2023-02-04 23:16:14 +0400

    Push done 2

--- Assets/_Scripts/PushController.cs
+++ Assets/_Scripts/PushController.cs
@@ -6,40 +6,39 @@ public class PushController : MonoBehaviour
 
     [SerializeField] private float distance = 1f;
     [SerializeField] private LayerMask boxMask;
-
+    [SerializeField] private float thrust = 1f;
+    
     private GameObject boxObject;
+
     #region Unity Methods
-
-
     private void Update()
     {
         Physics2D.queriesStartInColliders = false;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward * transform.localScale.x, distance, boxMask);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            UnAttachParent(hit);
-        }
-
-        if (hit.collider != null && hit.collider.CompareTag("Pushable") && Input.GetKey(KeyCode.E) && !Input.GetKey(KeyCode.Space))
-        {
+        if (hit.collider != null && hit.collider.CompareTag("Pushable") && Input.GetKey(KeyCode.Q))
+        {            
             boxObject = hit.collider.gameObject;
-            boxObject.transform.parent = this.transform;
 
-        }
-        else if (Input.GetKeyUp(KeyCode.E))
-        {
-            UnAttachParent(hit);
+            Rigidbody2D rb2D = boxObject.GetComponent<Rigidbody2D>();
+            Vector2 side=Vector2.zero;
+            
+            float playerX = transform.position.x;
+            float objectX = boxObject.transform.position.x;
+
+            if (playerX < objectX)
+            {
+                side = Vector2.right;
+            }
+
+            if (playerX > objectX)
+            {
+                side = Vector2.left;
+            }
+
+            rb2D.AddForce(side * thrust, ForceMode2D.Impulse);
         }
     }
     #endregion
 
-    private void UnAttachParent(RaycastHit2D hit)
-    {
-        if (hit.collider != null)
-        {
-            boxObject = hit.collider.gameObject;
-            boxObject.transform.parent = null;
-        }
-    }
 }
