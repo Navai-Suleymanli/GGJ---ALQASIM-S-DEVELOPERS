661d7fffb9076b2625c0920f9ff66116ea3a0173
Navai Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2023-02-04 22:11:34 +0400

    PickupCodes

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -1,17 +1,52 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Pickup : MonoBehaviour
 {
     public bool hasComponent;
+    public TextMeshProUGUI compCount;
+    public int collectedCompoinentCount = 0;
+    public GameObject tree;
+   
+
+
+
+
+    private void Start()
+    {
+
+    }
+
+
 
     
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Update()
     {
-        if (other.CompareTag("Component"))
+        Collider2D collider = Physics2D.OverlapCircle(transform.position, 0.5f, LayerMask.GetMask("WaterComponent"));
+
+        compCount.text = "Components Collected " + collectedCompoinentCount;
+
+        if (collider != null)
         {
-            hasComponent = true;
-            Destroy(gameObject);
+            Destroy(collider.gameObject);
+
+            collectedCompoinentCount++;
+
+            // UpdateScore(collectedCompoinentCount);
+        }
+
+        Collider2D collider2 = Physics2D.OverlapCircle(transform.position, 0.5f, LayerMask.GetMask("Tree"));      
+        
+        if (collider2 != null && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(collider2.gameObject);
+            Instantiate(tree, collider2.transform.position, collider2.transform.rotation);
         }
     }
+
+
+
+
 }
